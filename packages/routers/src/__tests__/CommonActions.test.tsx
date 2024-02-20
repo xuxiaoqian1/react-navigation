cfd83ca87bfacb635954bfa1e5651a7c5e76a3d5
Louis-C7 <chenlixi1@huawei.com>
2024-02-20 10:54:43 +0800

    sig format

--- packages/routers/src/__tests__/CommonActions.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as CommonActions from '../CommonActions';
-
-it('throws if NAVIGATE is called without key or name', () => {
-  // @ts-expect-error: we're explicitly using an invalid argument here
-  expect(() => CommonActions.navigate({})).toThrowError(
-    'You need to specify name or key when calling navigate with an object as the argument.'
-  );
-});
